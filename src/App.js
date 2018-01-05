fc82811e1d7c6b10465329f6fc8bdeddaa168f28
ShaToya Jones <shatoyajones@ShaToyas-MacBook-Pro.local>
2018-01-05 17:04:02 -0500

    added a shuffle algorithm to app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Section from './components/Section';
 import cards from './cards.json';
 import Footer from './components/Footer';
+// import Cards from './components/Cards';
 // import {Button, Icon} from 'react-materialize';
 
 class App extends Component {
@@ -17,6 +18,25 @@ class App extends Component {
     topScore: 0
   };
 
+  // componentWillMount() {
+  //   res => this.setState({cards:res.data.results})
+  //   .catch(err => console.log(err));
+  // }
+
+  // handleClick() {
+
+  // }
+
+  //shuffle algorithm taken from Stack Overflow (https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array).
+  shuffle = (a) => {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+}
 
   render() {
     return (
