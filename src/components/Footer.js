ab99fece3252e3c4200590b14728f83bd0ebc1f7
ShaToya Jones <shatoyajones@ShaToyas-MacBook-Pro.local>
2018-01-05 19:02:57 -0500

    modified footer.js, footer.css -- still working on functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,12 @@
 import React from "react";
 import "../styles/Footer.css";
 
-const styles = {
-    footerStyle: {
-        background: "black"
-    }
-};
-
 const Footer = props => (
     <footer className="footer">
-    <h1 style={styles.footerStyle}>TESTING</h1>
+    <div className={"bottom"} style={{ background: "#fff" }}> "Clicky Game! "
+    <img alt="react" src="assets/images/react.svg"></img>
+    </div>
     </footer>
-
 );
 
 export default Footer;
